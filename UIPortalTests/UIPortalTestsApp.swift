a38431b7d580653195dc942033d1f36b60ba4183
Aether <64797587+Aeastr@users.noreply.github.com>
2025-10-23 12:03:05 -0400

    Initial Commit

---
+++ UIPortalTests/UIPortalTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPortalTestsApp.swift
+//  UIPortalTests
+//
+//  Created by Aether on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UIPortalTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
